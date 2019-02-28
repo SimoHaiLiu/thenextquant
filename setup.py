6ff4b574f969bb0e3c195565278958dd7eb494f5
huangtao <huangtao@ifclover.com>
2019-02-28 15:46:05 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="thenextquant",
-    version="0.0.1",
+    version="0.0.2",
     packages=["quant",
               "quant.utils",
               ],
