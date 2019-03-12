e9f47a44d86484bcd8a851870a6f72f5f413a747
huangtao <huangtao@ifclover.com>
2019-03-12 15:31:05 +0800

    tag new version: v0.0.3

--- quant/__init__.py
+++ quant/__init__.py
@@ -7,5 +7,5 @@ Author: HuangTao
 Date:   2017/04/26
 """
 
-
-version = (0, 0, 1)
+__author__ = "HuangTao"
+__version__ = (0, 0, 3)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="thenextquant",
-    version="0.0.2",
+    version="0.0.3",
     packages=["quant",
               "quant.utils",
               ],
